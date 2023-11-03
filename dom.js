416964961612c8636a5133bc911285be40d2f810
Emon <emon22205101732@diu.edu.bd>
2023-11-03 11:22:10 +0600

    update

---
+++ dom.js
@@ -0,0 +1,24 @@
+
+const x=document.querySelectorAll('div');
+x.forEach(function(element){
+    element.innerText="hello world";
+});
+
+
+
+
+// const x=document.querySelector('div');
+// x.innerText="Emon"
+
+// const x=document.querySelector('div');
+// const str=x.innerText;
+// console.log(str);
+
+
+// console.log(document.title);
+
+// const user={
+//     name:"Emon3"
+// }
+// user.name="Md Emon"
+// console.log(user.name);
